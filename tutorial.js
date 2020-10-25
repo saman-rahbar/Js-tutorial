645a9d1c43a24418a8f15534c6bff61dd6575e96
Saman Rahbar <samanrahbar@Sam.hitronhub.home>
2020-10-25 01:52:26 -0700

    First tutorial

---
+++ tutorial.js
@@ -0,0 +1,15 @@
+/*
+Chapter 1
+Intoruduction to JS
+Tutor: Sam Rahbar
+Email:srahbar12@hotmail.com
+*/
+
+// this is how you define a variable without assignments:
+var x, y, z;
+x = 5;
+y = 12;
+z = 32;
+
+sum = x + y + z;
+console.log(sum)
